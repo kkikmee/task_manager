028886ba866702dbf0ab4da05a4686694daa73a8
kkikmee <deeed0108@gmail.com>
2025-12-25 02:58:21 +0500

    db

--- taskManager/taskManager/settings.py
+++ taskManager/taskManager/settings.py
@@ -78,10 +78,10 @@ WSGI_APPLICATION = 'taskManager.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/taskmanager',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
